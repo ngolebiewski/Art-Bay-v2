edd691e8a785834d7fe6ecef48b74cf008abf07a
Nick Golebiewski <nick.golebiewski@gmail.com>
2023-12-20 15:23:09 -0500

    Added Checkout route and component

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -7,6 +7,7 @@ import LoginForm from './LoginForm.jsx';
 import Register from './Register.jsx';
 import Header from './Header.jsx';
 import Cart from './Cart.jsx';
+import Checkout from './Checkout.jsx';
 
 const App = () =>{
 
@@ -23,6 +24,7 @@ const App = () =>{
           <Route path='/artwork' element={<Artwork />} />
           <Route path='/artwork/:id' element={<ArtworkDetail />} /> 
           <Route path='/cart' element={<Cart />} />
+          <Route path='/checkout' element={<Checkout />} />
           <Route path='/login' element={<LoginForm />} />
           <Route path='/register' element={<Register />} />
 
