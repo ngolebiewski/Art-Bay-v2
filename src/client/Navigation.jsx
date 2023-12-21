8e94309856e59d7a96478829dbe82b472b0d09f9
Nick Golebiewski <nick.golebiewski@gmail.com>
2023-12-21 12:26:00 -0500

    added Google Font Rethink Sans, deleted stock CSS and added in minimal CSS, created Nav links, basic header

--- src/client/Navigation.jsx
+++ src/client/Navigation.jsx
@@ -1,8 +1,21 @@
 import { Link } from "react-router-dom"
 
+// TODO: Add in welcome line: ie. welcome Dave...or welcome Guest (text tbd)
+// TODO: Add in token logic to show login/logout/cart
+// TODO: CSS
+
 const Navigation = () => {
   return(
-  <h2>Hi I'm the Nav Bar</h2>
+    <>
+  <nav>
+    <Link className='nav-link' to='/'>Home</Link>
+    <Link className='nav-link' to='/artwork'>Browse All</Link>
+    <Link className='nav-link' to='/login'>Login</Link>
+    <Link className='nav-link' to='/register'>Register</Link>
+    <Link className='nav-link' to='/cart'>Cart</Link>
+    <Link className='nav-link' to='/checkout'>Checkout</Link>
+  </nav>
+  </>
   )
 }
 
