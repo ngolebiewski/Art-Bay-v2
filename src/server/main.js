e6a5d20972f8864832c8018beb07ba32fbabf75e
Nick Golebiewski <nick.golebiewski@gmail.com>
2023-12-21 15:35:01 -0500

    Added index.js for API_Routes and art GET routes

--- src/server/main.js
+++ src/server/main.js
@@ -1,7 +1,39 @@
-import express from "express";
-import ViteExpress from "vite-express";
+const express = require("express");
+const ViteExpress = require("vite-express");
+const morgan = require('morgan');
+const jwt = require('jsonwebtoken');
 
 const app = express();
+
+
+//logging middleware 
+app.use(morgan("dev"));
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//token middleware
+app.use((req, res, next) => {
+  const bearerInfo = (req.headers.authorization)
+
+  if (!bearerInfo) req.user = null;
+  else if (bearerInfo.includes("Bearer ")) {
+    const tokenSplit = bearerInfo.split("Bearer ");
+    const token = tokenSplit[1]
+    try {req.user = jwt.verify(token, process.env.JWT_SECRET)}
+    catch(error) {req.user = null};
+  }
+  else req.user = null;
+  console.log("USER: ", req.user);
+  next()
+});
+
+
+//API ROUTES
+app.use('/api', require("./API_routes/index.js"))
+// app.use('/auth', require("./auth/auth.js"))
+
 
 app.get("/hello", (req, res) => {
   res.send("Hello Vite + React!");
