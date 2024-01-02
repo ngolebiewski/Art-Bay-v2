0a22c1f8a6d2728732348a7f0973794d544112d0
Nick Golebiewski <nick.golebiewski@gmail.com>
2024-01-02 14:13:44 -0500

    Added Art p ost, update (put), delete API endpoints

--- src/server/API_routes/artRoute.js
+++ src/server/API_routes/artRoute.js
@@ -43,5 +43,88 @@ router.get('/:id', async (req, res, next) => {
 });
 
 
+// api/art -> POST add new artwork *ADMIN ONLY
+router.post('/', async (req, res, next) => {
+  // Give non-users the "non-authorized" boot
+  if (!req.user) {res.sendStatus(401); 
+    return}
+
+  try {
+     const newArtwork = await prisma.art.create({
+      data: {
+        "title":req.body.title, 
+        "description":req.body.description,
+        "price":req.body.price, 
+        "inStock":req.body.inStock,    
+        "dimensions":req.body.dimensions,
+        "year":req.body.year,            
+        "imgUrl":req.body.imgUrl,        
+        "medium":req.body.medium,     
+        "artistId":req.body.artistId,
+      }
+     });
+    res.send(newArtwork);
+  } catch (error) {
+    console.error(error)
+    next(error);
+  }
+});
+
+//////////////////////////////////////////////////////
+// PUT
+// ROUTE(S)
+//////////////////////////////////////////////////////
+
+// api/art/:id -> PUT update artwork *ADMIN ONLY
+router.put('/:id', async (req, res, next) => {
+  // Give non-users the "non-authorized" boot
+  if (!req.user) {res.sendStatus(401); 
+    return}
+
+  try {
+    const artId = +req.params.id
+    const postBody = req.body
+
+    const updateArtwork = await prisma.art.update({
+      where:{
+        "id":artId
+      },
+      data:postBody,
+     });
+    res.send(updateArtwork);
+  } catch (error) {
+    console.error(error)
+    next(error);
+  }
+});
+
+
+//////////////////////////////////////////////////////
+// DELETE
+// ROUTE(S)
+//////////////////////////////////////////////////////
+
+// api/art/:id ->DELETE remove new artwork *ADMIN ONLY
+router.delete('/:id', async (req, res, next) => {
+  // Give non-users the "non-authorized" boot
+  if (!req.user) {res.sendStatus(401); 
+    return}
+
+  try {
+    const artId = +req.params.id
+    
+    const deleteArtwork = await prisma.art.delete({
+      where:{
+        "id":artId
+      },
+     });
+    res.send(deleteArtwork);
+  } catch (error) {
+    console.error(error)
+    next(error);
+  }
+});
+
+
 
 module.exports = router;
