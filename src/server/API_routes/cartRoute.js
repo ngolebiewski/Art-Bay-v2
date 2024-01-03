5bf8deb137dacfc464b8f0d4fc803eb3935733bf
Nick Golebiewski <nick.golebiewski@gmail.com>
2024-01-02 22:14:00 -0500

    fixed get cart route, added add item Post route

--- src/server/API_routes/cartRoute.js
+++ src/server/API_routes/cartRoute.js
@@ -4,17 +4,35 @@ const router = express.Router();
 const prisma = require("../client");
 
 // X   GET api/cart --> gets the current cart for the user (Need a token)
-// POST api/cart/ --> Add product to cart
+//  POST api/cart/ --> Add product to cart
 // PUT api/cart/:id --> Add product to cart
 // DELETE api/cart/:id --> Delete item from cart
 
 
+//TO DO!
 //get guest cart from local storage
 //logic in front end?
 //TBD
 
+const makeCart = async () => {
+  console.log("hi")
+    try {const newCart = await prisma.orderDetail.create({
+      data: {
+        isComplete: false,
+        userId: req.user.id,
+      },
+    });
+    console.log(newCart)
+    console.log()
+    return res.send(newCart);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+}
+
 // ROUTE: api/cart --> gets the current cart for the user (Need a token)
-router.get("/", async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   if (!req.user) {
     return res
       .status(401)
@@ -25,7 +43,6 @@ router.get("/", async (req, res, next) => {
 
   //see if the logged in user has an active cart (aka orderDetail) =>
     // is there an orderDetail with their id & a 'false' for is complete?
-    // If not make a new cart that will be false.
   try {
     const userCart = await prisma.orderDetail.findMany({
       where: {
@@ -34,30 +51,53 @@ router.get("/", async (req, res, next) => {
       },
     });
 
-    if (userCart) {
+    if (userCart.length > 0 ) {
       return res.send(userCart);
     }
 
-    // If no active cart, create a new one
+    //If no active cart, create a new one
     const newCart = await prisma.orderDetail.create({
       data: {
         isComplete: false,
         userId: req.user.id,
       },
     });
-
     return res.send(newCart);
+     
   } catch (error) {
     console.error(error);
     next(error);
   }
 });
 
-// PUT api/cart/:id --> Add product to cart
-//look got userID
+//  POST api/cart/:id/:quantity --> Add product(s) to cart
+router.post('/', async (req, res, next) => {
+//need to send this the art id, qty, and cart id in req.body -> Front End stuff
+//orderID *IS* the cart Id
+//LOGIC: assume the username/cart/etc. are already vetted...
+  const { artId, quantity, orderId } = req.body;
+  
+  try{ const addedItem = await prisma.cartItem.create({
+    data: {
+      quantity,
+      artId,
+      orderId,
+    }
+  })
 
-// router.get('/', async (req, res, next) =>
+  return res.send(addedItem);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 
-// ROUTE: api/cart
+
+
+
+
+
+
+
 
 module.exports = router;
